39ee9185fce98c794868d9f7db5bd2ee068fef99
Puskar-Roy <puskarroy600@gmail.com>
2024-05-19 22:48:55 +0530

    feat : change folder

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Navbar /> */}
         {children}
       </body>
     </html>
