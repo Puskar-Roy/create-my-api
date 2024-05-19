150abad2739ec0922b198952b1923899369af0c4
Puskar-Roy <puskarroy600@gmail.com>
2024-05-19 10:46:26 +0530

    chore : tailwind config

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,5 +1,12 @@
 import type { Config } from "tailwindcss";
 
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +22,24 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+
+
+
+
 export default config;
