4755aced26fd64d0d41546d2e82bb7f21ae8404c
Puskar-Roy <puskarroy600@gmail.com>
2024-05-19 18:53:21 +0530

    fix : deployment

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {Navbar} from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
