c37c5a1906002d952ca11be49c6f5ce7abad1097
Puskar-Roy <puskarroy600@gmail.com>
2024-05-20 12:24:48 +0530

    feat : package

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -29,7 +29,7 @@ function Gradient({
 
 const LINKS = [
   {
-    title: "Docs",
+    title: "Docsds",
     href: "https://turbo.build/repo/docs",
     description: "Find in-depth information about Turborepo features and API.",
   },
