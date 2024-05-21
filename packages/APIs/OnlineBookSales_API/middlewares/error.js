e19aabf82ed5fd9b4e6b683a3572ca67b877645c
Puskar-Roy <puskarroy600@gmail.com>
2024-05-21 20:33:16 +0530

    feat : new api

---
+++ packages/APIs/OnlineBookSales_API/middlewares/error.js
@@ -0,0 +1,47 @@
+const ErrorHandler = require("../utils/errorHandler.js");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  // Handle undefined payload error
+  if (err.message === "payload is not defined") {
+    const message = "Payload is not defined";
+    err = new ErrorHandler(message, 500);
+  }
+
+  // Handle undefined jwtSecret error
+  if (err.message === "jwtSecret is not defined") {
+    const message = "JWT secret key is not defined";
+    err = new ErrorHandler(message, 500);
+  }
+
+  // Wrong Mongodb Id error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT EXPIRE error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is Expired, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
