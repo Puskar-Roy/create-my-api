d746e9f7153723823d8b2e8b2b84a2c57b4a52e5
Puskar-Roy <puskarroy600@gmail.com>
2024-05-21 16:48:45 +0530

    feat : docs page

--- apps/web/app/(docs)/docs/page.tsx
+++ apps/web/app/(docs)/docs/page.tsx
@@ -24,13 +24,19 @@ const page = () => {
               <span className={`text-orange-500 font-extrabold `}>My</span>{" "}
               <span className={`text-red-500 font-extrabold `}>API</span>
             </div>
-            Docs Page coming soon 👻
+            Docs Page coming soon 👻{" "}
           </div>
           <div className="flex justify-center items-center">
             <Link href="https://github.com/Puskar-Roy/create-my-api">
               <GIthubButton />
             </Link>
           </div>
+          <Link
+            href="/"
+            className="bg-gradient-to-r from-gray-800 to-black px-4 py-2 rounded-full text-sm text-slate-300 z-10 border border-gray-600 hover:scale-105 duration-200 hover:border-gray-800 hover:from-black hover:to-gray-900 hover:text-white"
+          >
+            Go Home
+          </Link>
         </div>
       </div>
     </div>
--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -142,7 +142,7 @@ const page = () => {
           }}
         />
 
-        <div>
+        <div className="flex justify-center items-center gap-5 flex-col">
           <Image
             className="animate-float"
             src={logo}
@@ -150,7 +150,7 @@ const page = () => {
             width={400}
             alt="Logo"
           />{" "}
-          <div className="font-bold text-center text-slate-300">
+          <div className="font-bold text-center  text-slate-300">
             <div>
               <span className={`text-orange-300 font-extrabold `}>Create</span>{" "}
               <span className={`text-orange-500 font-extrabold `}>My</span>{" "}
@@ -163,6 +163,12 @@ const page = () => {
               <GIthubButton />
             </Link>
           </div>
+          <Link
+            href="/"
+            className="bg-gradient-to-r from-gray-800 to-black px-4 py-2 rounded-full text-sm text-slate-300 z-10 border border-gray-600 hover:scale-105 duration-200 hover:border-gray-800 hover:from-black hover:to-gray-900 hover:text-white"
+          >
+            Go Home
+          </Link>
         </div>
       </div>
     </div>
