489bc055128dee48e20c57a89070311a2866e04e
Puskar-Roy <puskarroy600@gmail.com>
2024-06-10 21:23:49 +0530

    feat : change start feature

--- apps/web/components/ui/GIthub-Button.tsx
+++ apps/web/components/ui/GIthub-Button.tsx
@@ -43,7 +43,7 @@ const GIthubButton = () => {
         points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
       ></polygon>
     </svg>
-    <span className="text-white ml-2">01</span>
+    <span className="text-white ml-2">09</span>
   </span>
 </button>
 
