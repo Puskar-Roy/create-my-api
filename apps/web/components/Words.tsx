5d0f1b18ff36138b3eaad91d1f11c08f41876734
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-10-12 19:22:01 +0530

    fixed UI inconsistency

--- apps/web/components/Words.tsx
+++ apps/web/components/Words.tsx
@@ -43,7 +43,7 @@ export function FlipWordsDemo() {
         <div className="flex justify-center text-start md:flex-row gap-5 flex-row sm:flex-row">
 
           <Link href="/docs">
-            <button className="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-[0.85rem] rounded-full border border-gray-600 hover:scale-105 duration-200 hover:border-white hover:from-red-700 hover:to-red-600 z-10">
+            <button className="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-[0.85rem] rounded-full border border-gray-600 hover:scale-105 duration-200 hover:border-gray-800 hover:from-red-700 hover:to-red-600 z-10">
               <FaArrowCircleRight className="text-2xl" />
             </button>
           </Link>
@@ -51,7 +51,7 @@ export function FlipWordsDemo() {
 
 
           <Link href="https://www.npmjs.com/package/create-my-api">
-            <button className="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-4 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-white hover:border-white hover:from-red-700 hover:to-red-600 z-10">
+            <button className="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-4 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-white hover:border-gray-800 hover:from-red-700 hover:to-red-600 z-10">
               <FaNpm className="text-5xl" />
             </button>
           </Link>
@@ -59,7 +59,7 @@ export function FlipWordsDemo() {
 
 
           <Link href="https://github.com/Puskar-Roy">
-            <button className="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-[0.85rem] rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900 z-10">
+            <button className="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-[0.85rem] rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-red-700 hover:to-red-600 z-10">
               <Github />
             </button>
           </Link>
